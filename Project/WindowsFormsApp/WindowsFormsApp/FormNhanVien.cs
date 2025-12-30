1f9bec80e707d9d0ae5847d6a7961b188ec0ed00
hviet28102005-cmyk <hviet28102005@gmail.com>
2025-12-30 10:35:34 +0700

    Add files via upload

---
+++ Project/WindowsFormsApp/WindowsFormsApp/FormNhanVien.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class FormNhanVien : Form
+    {
+        string _maNV;
+        DataTable dtGioHang = new DataTable();
+
+        public FormNhanVien(string maNV)
+        {
+            InitializeComponent();
+            _maNV = maNV;
+        }
+
+        private void FormNhanVien_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void sảnPhẩmToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormSanPhamChu F = new FormSanPhamChu();
+            F.MdiParent = this;
+            F.Show();
+        }
+
+        private void thanhToánToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FormThanhToan T = new FormThanhToan(dtGioHang);
+            T.MdiParent = this;
+            T.Show();
+        }
+    }
+
+}
